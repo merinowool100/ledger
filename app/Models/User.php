6ee10db46a99243a6ca6e018d37c0dc78cfba757
merinowool100 <masatinib@gmail.com>
2026-03-03 17:59:45 +0900

    error fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Account;
 
 class User extends Authenticatable
 {
@@ -49,6 +50,14 @@ class User extends Authenticatable
     public function ledgers()
     {
         return $this->hasMany(Ledger::class);
+    }
+
+    /**
+     * A user can have multiple accounts.
+     */
+    public function accounts()
+    {
+        return $this->hasMany(Account::class);
     }
 
 }
